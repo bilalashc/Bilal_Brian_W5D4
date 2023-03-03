113c8f802c267c54442f0de0b769b94ea5dde963
Brian Vo <brianhvo02@gmail.com>
2023-03-02 16:30:46 -0800

    Completed associations practice

--- practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
@@ -10,4 +10,21 @@
 #  updated_at    :datetime         not null
 #
 class Course < ApplicationRecord
+    has_many :enrollments,
+        primary_key: :id,
+        foreign_key: :course_id,
+        class_name: :Enrollment
+
+    has_many :enrolled_students, through: :enrollments, source: :student
+
+    belongs_to :prerequisite,
+        class_name: :Course,
+        foreign_key: :prereq_id,
+        primary_key: :id,
+        optional: true
+
+    belongs_to :instructor,
+        primary_key: :id,
+        foreign_key: :instructor_id,
+        class_name: :User
 end
